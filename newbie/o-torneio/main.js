2bcb75dd368b08882c4dce22ddf9b67f33a65ea3
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-05-08 18:21:42 -0300

    O torneio

---
+++ newbie/o-torneio/main.js
@@ -0,0 +1,12 @@
+let n1 = 300
+let n3 = 500
+let n2 = 1000
+let n4 = 350
+let n5 = 100
+
+let result = n1 + n2 + n3 + n4 + n5
+if (result > 5000) {
+    console.log('Acesso negado')
+} else {
+    console.log('Acesso liberado')
+}
