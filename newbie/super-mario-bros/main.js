246e70c7b7c75420f1332fd60d16185500e50b85
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-05-08 21:53:13 -0300

    Super Mario Bros

---
+++ newbie/super-mario-bros/main.js
@@ -0,0 +1,13 @@
+let sc = 10
+let mm = 4
+let ck = 0
+
+if (sc > 29) {
+    console.log('PROXIMO MUNDO')
+} else {
+    sc = 30 - sc
+    mm = 6 - mm
+    ck = 3 - ck
+
+    console.log(sc, mm, ck)
+}
