e23a4f5684356c56ca0e24c020ed34eab87ef11c
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-05-11 16:54:54 -0300

    Quem é o mais forte

---
+++ newbie/quem-e-o-mais-forte/main.js
@@ -0,0 +1,10 @@
+let lux = 350
+let ekko = 350
+
+if (lux == ekko) {
+    console.log(ekko, 'Os dois tem a mesma quantidade de poder')
+} else if (lux > ekko) {
+    console.log(lux, "Lux")
+} else {
+    console.log(ekko, "Ekko")
+}
