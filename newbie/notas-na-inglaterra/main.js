67ed4da2fa9b701502ce659611a4b34ebf9fe3d3
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-05-15 14:58:20 -0300

    Notas na Inglaterra

---
+++ newbie/notas-na-inglaterra/main.js
@@ -0,0 +1,16 @@
+let nota = 87
+
+if (nota > 85){
+    console.log('A')
+} 
+else if (nota > 60){
+    console.log('B')
+}
+else if (nota > 35){
+    console.log('C')
+}
+else if (nota > 1) {
+    console.log('D')
+} else {
+    console.log('E')
+}
