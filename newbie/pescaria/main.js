9ecd73d22ddac4ae104747467806b0125d4c5625
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-05-15 14:45:11 -0300

    Pescaria

---
+++ newbie/pescaria/main.js
@@ -0,0 +1,13 @@
+let peixes = 17
+let valorPeixe = 450
+let total = peixes * valorPeixe
+
+if (total < 500) {
+    console.log('Paciencia Firmino!')
+} else if (total < 1800) {
+    console.log('Vara de Bambu')
+} else if (total < 7500) {
+    console.log('Vara de Fibra de Vidro')
+} else {
+    console.log('Vara de Irídio')
+}
