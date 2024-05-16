cb189f0ea9ea388ba6695f8de7573d2acdbaf457
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-05-16 09:23:21 -0300

    Ingressos Promocionais

---
+++ newbie/ingressos-promocionais/main.js
@@ -0,0 +1,19 @@
+let ingresso = 'idoso'
+
+switch (ingresso){
+    case 'idoso':
+        console.log('gratuidade')
+        break;
+    case 'estudante':
+        console.log('10 reais')
+        break;
+    case 'casadinha':
+        console.log('30 reais')
+        break;
+    case 'trio':
+        console.log('40 reais')
+        break;
+    default: 
+        console.log('20 reais')
+        break;
+}
