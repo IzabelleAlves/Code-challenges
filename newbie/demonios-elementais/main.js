027c602a60779584dabdd799856e2c861a10052d
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-05-17 15:44:43 -0300

    Demonios Elementais

---
+++ newbie/demonios-elementais/main.js
@@ -0,0 +1,18 @@
+let type = 'Demônio do fogo'
+
+switch (type){
+    case 'Demônio do fogo':
+        console.log('A fraqueza do demônio é: água')
+        break;
+    case 'Demônio do gelo':
+        console.log('A fraqueza do demônio é: fogo')
+        break;
+    case 'Demônio da fumaça':
+        console.log('A fraqueza do demônio é: vento')
+        break;
+    case 'Demônio da escuridão':
+        console.log('A fraqueza do demônio é: luz')
+        break;
+    default:
+        console.log('Não foi possível identificar a fraqueza do demônio')
+}
