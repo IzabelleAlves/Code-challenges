c81468a30461bfd5c0c09276d76400b65592c858
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-05-17 16:09:37 -0300

    Continha

---
+++ newbie/continha/main.js
@@ -0,0 +1,9 @@
+let a = 7 
+let b = 3 
+let c = 15 
+let d = 30
+let e = 0 
+let f = 2
+
+let result = ((a + b) * (c - d) * (e + f)) / 2.0 
+console.log('Eu sou FERA nas continhas e o resultado é', result.toFixed(1))
