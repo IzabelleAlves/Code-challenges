208acb6cf21548161e2811c04114942a65113a43
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-05-20 10:07:47 -0300

    As novas missões Jedy

---
+++ newbie/as-novas-missoes-jedy/main.js
@@ -0,0 +1,8 @@
+let n = 3
+let xp = 50
+let somaXp = n * xp
+let yoda = 1500
+let luke = 800
+let ahsoka = 1000
+
+console.log(`Yoda ${somaXp + yoda} Luke ${somaXp + luke} Ahsoka ${somaXp + ahsoka}`)
