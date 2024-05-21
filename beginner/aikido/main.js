164365e67e433ec2efa596eedc84c5b924e7d1fa
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-05-21 09:39:17 -0300

    Aikido

---
+++ beginner/aikido/main.js
@@ -0,0 +1,24 @@
+let x = 2
+let y = 30
+
+if (x < 1) {
+    console.log('branca')
+} 
+else if (y >= 120) {
+    console.log('marrom')
+} 
+else if (y >= 110) {
+    console.log('azul')
+} 
+else if (y >= 90) {
+    console.log('verde')
+} 
+else if (y >= 72) {
+    console.log('roxa')
+} 
+else if (y >= 60) {
+    console.log('amarela')
+} 
+else {
+    console.log('branca')
+}
