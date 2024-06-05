ff352761ecad1ff1c283f5afa1ceca26f6ec760e
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-06-05 08:06:59 -0300

    Degradação de pneus na F1

--- beginner/degradacao-de-pneus-F1/main.js
+++ beginner/degradacao-de-pneus-F1/main.js
@@ -1,14 +1,14 @@
 let tireType = 'S'
-let degradationRatedS = 4.8
-let degradationRatedM = 3.7 
-let degradationRatedH = 2.6
+let degradationRateS = 4.8
+let degradationRateM = 3.7 
+let degradationRateH = 2.6
 
 if (tireType == 'S') {
-  console.log(`${Math.floor(80 / degradationRatedS)}`)
+  console.log(`${Math.floor(80 / degradationRateS)}`)
 } else if (tireType == 'M') {
-  console.log(`${Math.floor(80 / degradationRatedM)}`)
+  console.log(`${Math.floor(80 / degradationRateM)}`)
 } else if (tireType == 'H') {
-  console.log(`${Math.floor(80 / degradationRatedH)}`)
+  console.log(`${Math.floor(80 / degradationRateH)}`)
 } else {
   console.log(`Invalid tire type`)
 }
