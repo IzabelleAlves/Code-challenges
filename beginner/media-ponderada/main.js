dc975e060d135e7393b31fee333335a769d61b49
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-06-10 09:46:21 -0300

    Media Ponderada

---
+++ beginner/media-ponderada/main.js
@@ -0,0 +1,10 @@
+let grade1 = 8.0
+let grade2 = 7.5
+let finalWork = 9.0
+
+const weightExam = 4
+const weightfinalWork = 2
+
+let average = (grade1 * weightExam + grade2 * weightExam + finalWork * weightfinalWork) / (weightExam + weightExam + weightfinalWork)
+
+console.log(average)
