81c3a53c423408329cfb7d6a0f907e919c8dad25
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-07-02 19:54:51 -0300

    Vendas Online

---
+++ beginner/vendas-online/main.js
@@ -0,0 +1,9 @@
+let produto1 = 2 
+let produto2 = 2
+let produto3 = 10
+
+let valor1 = 10.99 
+let valor2 = 5.50
+let valor3 = 15.75
+
+console.log(`Total da compra: ${((produto1 * valor1) + (produto2 * valor2) + (produto3 * valor3)).toFixed(2)}`);
