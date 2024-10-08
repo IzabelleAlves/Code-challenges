835977ad2f5d026dfb388c0dd1d7bc568b7e9f59
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-10-08 19:54:09 -0300

    Ajuste na lógica do cálculo da linha

--- amateur/campo-de-minhocas/main.js
+++ amateur/campo-de-minhocas/main.js
@@ -1,5 +1,3 @@
-// let matriz = [[100, 110, 0, 100]];
-
 let matriz = [
   [81, 28, 240, 10],
   [40, 10, 100, 240],
@@ -11,18 +9,21 @@ let maiorSomaLinha = 0;
 let somaColuna = 0;
 let somaLinha = 0;
 
+for (let linha = 0; linha < matriz.length; linha++) {
+  somaLinha = matriz[linha].reduce((a, b) => a + b);
+  if (somaLinha > maiorSomaLinha) {
+    maiorSomaLinha = somaLinha;
+  }
+}
+
 for (let coluna = 0; coluna < matriz[0].length; coluna++) {
   somaColuna = 0;
 
   for (let linha = 0; linha < matriz.length; linha++) {
-    somaLinha = matriz[linha].reduce((a, b) => a + b);
     somaColuna += matriz[linha][coluna];
 
     if (somaColuna > maiorSomaColuna) {
       maiorSomaColuna = somaColuna;
-    }
-    if (somaLinha > maiorSomaLinha) {
-      maiorSomaLinha = somaLinha;
     }
   }
 }
