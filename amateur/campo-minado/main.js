0cbfa2617be280db596343b68f169820894638d2
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-12-28 10:22:28 -0300

    Campo Minado

---
+++ amateur/campo-minado/main.js
@@ -0,0 +1,31 @@
+// let tabuleiro =  [0, 1, 1, 1, 0]
+let tabuleiro =  [0, 1, 1, 0, 1]
+//se tiver 0, n tem nd se tiver 1 tem algo
+let tabuleiroAtualizado = []
+let contadorMinas = 0
+
+for (let i = 0; i < tabuleiro.length; i++){
+  if (tabuleiro[i] == 1){
+    contadorMinas++
+    if (tabuleiro[i + 1] == 1){
+      contadorMinas++
+    }
+    if (tabuleiro[i - 1] == 1){
+      contadorMinas++
+    }
+  }
+
+  else if (tabuleiro[i] == 0){
+    if (tabuleiro[i + 1] == 1){
+      contadorMinas++
+    }
+    if (tabuleiro[i - 1] == 1){
+      contadorMinas++
+    }
+  }
+
+  tabuleiroAtualizado.push(contadorMinas)
+  contadorMinas = 0
+}
+
+console.log(tabuleiroAtualizado)
